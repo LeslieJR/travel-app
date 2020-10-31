05e1f1e6616e7f7c44e95d6e875d8d78cc5ce6fd
Leslie Reyes <l.reyes.ubiqum@gmail.com>
2020-10-31 17:31:37 +0100

    icons in sidebar nav

--- src/components/NavigationItems/NavigationItems.js
+++ src/components/NavigationItems/NavigationItems.js
@@ -5,9 +5,9 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" exact>Home</NavigationItem>
-        <NavigationItem link="/about">About</NavigationItem>
-        <NavigationItem link="/tours">Tours</NavigationItem>
+        <NavigationItem link="/" exact><i  class="fas fa-home"></i> Home</NavigationItem>
+        <NavigationItem link="/about"><i  class="fas fa-info-circle"></i> About</NavigationItem>
+        <NavigationItem link="/tours"><i  class="fas fa-plane"></i> Tours</NavigationItem>
     </ul>
 )
 
